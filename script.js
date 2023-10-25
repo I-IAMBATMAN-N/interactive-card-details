9d423990474c9159fb77d1a4d042263cc6d0b9c7
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-10-25 17:23:02 +0200

    [UPDATE]Better Regular Expression check

--- script.js
+++ script.js
@@ -165,7 +165,9 @@ const confirmButton = document.querySelector("article form button");
 const thankYouSection = document.querySelector(".thank-you");
 
 const regExpNum = /[0-9]/;
-const regExpStr = /[a-zA-Z]/;
+
+const regExpStr = /[a-záéíóúàèìòùäëïöüěščřžýůú=+-.,]/;
+const regExpStr2 = /[A-ZÁÉÍÓÚÀÈÌÒÙÄËÏÖÜĚŠČŘŽÝŮÚ=+-.,]/;
 
 let elArr = [form, thankYouSection];
 
@@ -176,16 +178,16 @@ confirmButton.addEventListener("click", function () {
   if (regExpNum.test(inputName.value)) {
     incorrectFields.push("Card name");
   }
-  if (regExpStr.test(inputNumber.value)) {
+  if (regExpStr.test(inputNumber.value) || regExpStr2.test(inputNumber.value)) {
     incorrectFields.push("Card number");
   }
-  if (regExpStr.test(inputMonth.value)) {
+  if (regExpStr.test(inputMonth.value) || regExpStr2.test(inputMonth.value)) {
     incorrectFields.push("Card month");
   }
-  if (regExpStr.test(inputYear.value)) {
+  if (regExpStr.test(inputYear.value) || regExpStr2.test(inputYear.value)) {
     incorrectFields.push("Card year");
   }
-  if (regExpStr.test(inputCvc.value)) {
+  if (regExpStr.test(inputCvc.value) || regExpStr2.test(inputCvc.value)) {
     incorrectFields.push("Card cvc");
   }
 
