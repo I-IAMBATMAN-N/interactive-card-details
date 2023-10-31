cca7efe9e46a83814be239341ac3f9f48ad3f269
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-10-31 21:37:58 +0100

    [UPDATE] key check function

--- script.js
+++ script.js
@@ -75,6 +75,11 @@ function cardInputChecker() {}
 // Card Name Input
 //////////////////////////////////////////////////////
 inputName.addEventListener("input", function (e) {
+  if (regExpNum.test(inputName.value)) {
+    alert("Incorrect key in Card name");
+    inputName.value = inputName.value.slice(0, -1);
+  }
+
   cardName.innerText = "";
   if (inputName.value.length === 0) {
     cardName.innerText = "";
@@ -93,6 +98,10 @@ inputName.addEventListener("input", function (e) {
 // Card Number Input
 //////////////////////////////////////////////////////
 inputNumber.addEventListener("input", function (event) {
+  if (regExpStr.test(inputNumber.value) || regExpStr2.test(inputNumber.value)) {
+    alert("Incorrect key in Card number");
+    inputNumber.value = inputNumber.value.slice(0, -1);
+  }
   if (
     inputNumber.value.length === 4 ||
     inputNumber.value.length === 9 ||
@@ -133,26 +142,44 @@ inputNumber.addEventListener("keydown", function (event) {
 //////////////////////////////////////////////////////
 
 inputMonth.addEventListener("input", function () {
+  //
+  if (regExpStr.test(inputMonth.value) || regExpStr2.test(inputMonth.value)) {
+    alert("Incorrect key in Card month");
+    inputMonth.value = inputMonth.value.slice(0, -1);
+  }
   if (inputMonth.value.length === 0) {
     cardMonth.innerHTML = "00";
+  } else if (inputMonth.value.length === 3) {
+    inputMonth.value = inputMonth.value.slice(0, -1);
   } else {
     const month = inputMonth.value;
     cardMonth.innerHTML = month;
   }
 });
-
 inputYear.addEventListener("input", function () {
+  //
+  if (regExpStr.test(inputYear.value) || regExpStr2.test(inputYear.value)) {
+    alert("Incorrect key in Card year");
+    inputYear.value = inputYear.value.slice(0, -1);
+  }
   if (inputYear.value.length === 0) {
     cardYear.innerHTML = "00";
+  } else if (inputYear.value.length === 3) {
+    inputYear.value = inputYear.value.slice(0, -1);
   } else {
     const year = inputYear.value;
     cardYear.innerHTML = year;
   }
 });
-
 inputCvc.addEventListener("input", function () {
+  if (regExpStr.test(inputCvc.value) || regExpStr2.test(inputCvc.value)) {
+    alert("Incorrect key in Card cvc");
+    inputCvc.value = inputCvc.value.slice(0, -1);
+  }
   if (inputCvc.value.length === 0) {
     cardCvc.innerHTML = "000";
+  } else if (inputCvc.value.length === 4) {
+    inputCvc.value = inputCvc.value.slice(0, -1);
   } else {
     const cvc = inputCvc.value;
     cardCvc.innerHTML = cvc;
@@ -175,21 +202,21 @@ confirmButton.addEventListener("click", function () {
   //
   const incorrectFields = [];
 
-  if (regExpNum.test(inputName.value)) {
-    incorrectFields.push("Card name");
-  }
-  if (regExpStr.test(inputNumber.value) || regExpStr2.test(inputNumber.value)) {
-    incorrectFields.push("Card number");
-  }
-  if (regExpStr.test(inputMonth.value) || regExpStr2.test(inputMonth.value)) {
-    incorrectFields.push("Card month");
-  }
-  if (regExpStr.test(inputYear.value) || regExpStr2.test(inputYear.value)) {
-    incorrectFields.push("Card year");
-  }
-  if (regExpStr.test(inputCvc.value) || regExpStr2.test(inputCvc.value)) {
-    incorrectFields.push("Card cvc");
-  }
+  // if (regExpNum.test(inputName.value)) {
+  //   incorrectFields.push("Card name");
+  // }
+  // if (regExpStr.test(inputNumber.value) || regExpStr2.test(inputNumber.value)) {
+  //   incorrectFields.push("Card number");
+  // }
+  // if (regExpStr.test(inputMonth.value) || regExpStr2.test(inputMonth.value)) {
+  //   incorrectFields.push("Card month");
+  // }
+  // if (regExpStr.test(inputYear.value) || regExpStr2.test(inputYear.value)) {
+  //   incorrectFields.push("Card year");
+  // }
+  // if (regExpStr.test(inputCvc.value) || regExpStr2.test(inputCvc.value)) {
+  //   incorrectFields.push("Card cvc");
+  // }
 
   if (incorrectFields.length) {
     alert(`Please fill corectly fields:\n${[...incorrectFields]}`);
